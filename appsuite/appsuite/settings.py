ffec9dec682fae010115c9dbd25a5475c3356321
Anthony Quivers <anthonyquivers@gmail.com>
2018-01-29 13:31:24 -0500

    Templates

--- appsuite/appsuite/settings.py
+++ appsuite/appsuite/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'appsuite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['notes/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- appsuite/notes/views.py
+++ appsuite/notes/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader, RequestContext
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello Notes")
+    #return HttpResponse("Hello Notes")
+    template = loader.get_template('index.html')
+    rc = RequestContext(request,{'user':request.user})
+    rc2 = {'username':'Anthony Quivers'}
+    return HttpResponse(template.render(rc2))
