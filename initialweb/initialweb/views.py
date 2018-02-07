bc0ca776bbf264a0b0c30947e9db22422b2a2acd
Anthony Quivers <anthonyquivers@gmail.com>
2018-02-07 17:27:30 -0500

    Boostrap Navbar Stopping Point

--- initialweb/initialweb/views.py
+++ initialweb/initialweb/views.py
@@ -7,6 +7,33 @@ class Article:
         self.title = title
         self.content = content
         
+def get_articles():
+    article1 = Article(
+                        id=1,
+                        title='Article1',
+                        content='''article 1 content.
+                        This is a multi-line peice of content
+                        This is the last line'''
+                        )
+
+    article2 = Article(
+                        id=2,
+                        title='Article2',
+                        content='''article 2 content.
+                        This is a multi-line peice of content
+                        This is the last line'''
+                        )
+
+    article3 = Article(
+                        id=3,
+                        title='Article3',
+                        content='''article 3 content.
+                        This is a multi-line peice of content
+                        This is the last line'''
+                        )
+    
+    return [article1, article2, article3]
+        
 def index(request):
     article1 = Article(
                         id=1,
@@ -43,13 +70,14 @@ def index(request):
 def about(request):
     content = {
         'current_date': datetime.now(),
-        'title':'about'
+        'title':'About'
     }   
     return render(request, 'about.html', content)
     
 def news(request):
     content = {
+        'articles': get_articles(),
         'current_date': datetime.now(),
-        'title':'news'
+        'title':'News'
     }  
     return render(request, 'news.html', content)
